44a58f8582419d9740d5791fef74cb936833ca39
Caleb Casapaico <caleb.casapaico@pucp.edu.pe>
2022-06-26 13:46:43 -0500

    ordenar funciones por:... LISTO

--- src/main/java/com/example/cinestudiar/daos/OperadorDao.java
+++ src/main/java/com/example/cinestudiar/daos/OperadorDao.java
@@ -8,103 +8,214 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class OperadorDao {
-    public ArrayList<BFuncion> TodasLasFunciones(){
-        ArrayList<BFuncion> todasLasFunciones = new ArrayList<>();
+    public ArrayList<BFuncion> filtradoFunciones(String filtro){
         String user = "root";
         String pass = "root";
         String url = "jdbc:mysql://localhost:3306/mysystem4?serverTimezone=America/Lima";
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+        if (filtro.equals("") || filtro.equals("defecto")){
+            ArrayList<BFuncion> todasLasFunciones = new ArrayList<>();
+            try {
+                String sql = "select f.idfuncion as 'IdFunción',p.nombre     as `Título de Película`,\n" +
+                        "f.fecha,f.hora,\n" +
+                        "se.nombre_sede as `Sede`,\n" +
+                        "sa.idsala as `Sala`,\n" +
+                        "f.precio_ticket as `Precio`,\n" +
+                        "round(p.calificacion) as `Calificacion`\n" +
+                        "from funciones f\n" +
+                        "inner join salas sa on (f.idsala=sa.idsala)\n" +
+                        "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
+                        "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
+                        "order by se.nombre_sede,sa.idsala,f.fecha desc;";
+                Connection conn = DriverManager.getConnection(url,user,pass);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                System.out.println("PORBAAAAAANDO SI LLEGGA");
+                while (rs.next()){
+                    BFuncion fu = new BFuncion();
+                    fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
+                    fu.setPelicula(rs.getString(2));
+                    fu.setFecha(rs.getString(3));
+                    fu.setHora(rs.getString(4));
+                    fu.setSede(rs.getString(5));
+                    fu.setIdSala(rs.getInt(6));
+                    fu.setPrecioTicket(rs.getInt(7));
+                    fu.setCalificacion(rs.getInt(8));
 
-        try {
-            String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
-                    "f.fecha,f.hora,\n" +
-                    "se.nombre_sede as `Sede`,\n" +
-                    "sa.idsala as `Sala`,\n" +
-                    "f.precio_ticket as `Precio`,\n" +
-                    "round(p.calificacion) as `Calificacion`\n" +
-                    "from funciones f\n" +
-                    "inner join salas sa on (f.idsala=sa.idsala)\n" +
-                    "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
-                    "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
-                    "order by se.nombre_sede,sa.idsala,f.fecha desc;";
-            Connection conn = DriverManager.getConnection(url,user,pass);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()){
-                BFuncion fu = new BFuncion();
-                fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
-                fu.setPelicula(rs.getString(2));
-                fu.setFecha(rs.getString(3));
-                fu.setHora(rs.getString(4));
-                fu.setSede(rs.getString(5));
-                fu.setIdSala(rs.getInt(6));
-                fu.setPrecioTicket(rs.getInt(7));
-                fu.setCalificacion(rs.getInt(8));
 
 
-
-                todasLasFunciones.add(fu);
+                    todasLasFunciones.add(fu);
+                }
+            } catch (SQLException e){
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
+
+            return todasLasFunciones;
         }
 
-        return todasLasFunciones;
-    }
-    public ArrayList<BFuncion> FuncionesDisponibles(){
-        ArrayList<BFuncion> funcionesDisponibles = new ArrayList<>();
-        String user = "root";
-        String pass = "root";
-        String url = "jdbc:mysql://localhost:3306/mysystem4?serverTimezone=America/Lima";
+        if (filtro.equals("Funciones Disponibles")){
+            ArrayList<BFuncion> funcionesDisponibles = new ArrayList<>();
+            try {
+                String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
+                        "f.fecha,f.hora,\n" +
+                        "se.nombre_sede as `Sede`,\n" +
+                        "sa.idsala as `Sala`,\n" +
+                        "f.precio_ticket as `Precio`,\n" +
+                        "round(p.calificacion) as `Calificacion`\n" +
+                        "from funciones f\n" +
+                        "inner join salas sa on (f.idsala=sa.idsala)\n" +
+                        "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
+                        "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
+                        "where datediff(f.fecha,now())>=0\n" +
+                        "order by se.nombre_sede,sa.idsala,f.fecha desc;";
+                Connection conn = DriverManager.getConnection(url,user,pass);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+                while (rs.next()){
+                    BFuncion fu = new BFuncion();
+                    fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
+                    fu.setPelicula(rs.getString(2));
+                    fu.setFecha(rs.getString(3));
+                    fu.setHora(rs.getString(4));
+                    fu.setSede(rs.getString(5));
+                    fu.setIdSala(rs.getInt(6));
+                    fu.setPrecioTicket(rs.getInt(7));
+                    fu.setCalificacion(rs.getInt(8));
 
-        try {
-            String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
-                    "f.fecha,f.hora,\n" +
-                    "se.nombre_sede as `Sede`,\n" +
-                    "sa.idsala as `Sala`,\n" +
-                    "f.precio_ticket as `Precio`,\n" +
-                    "round(p.calificacion) as `Calificacion`\n" +
-                    "from funciones f\n" +
-                    "inner join salas sa on (f.idsala=sa.idsala)\n" +
-                    "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
-                    "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
-                    "where datediff(f.fecha,now())>=0\n" +
-                    "order by se.nombre_sede,sa.idsala,f.fecha desc;";
-            Connection conn = DriverManager.getConnection(url,user,pass);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
-
-            while (rs.next()){
-                BFuncion fu = new BFuncion();
-                fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
-                fu.setPelicula(rs.getString(2));
-                fu.setFecha(rs.getString(3));
-                fu.setHora(rs.getString(4));
-                fu.setSede(rs.getString(5));
-                fu.setIdSala(rs.getInt(6));
-                fu.setPrecioTicket(rs.getInt(7));
-                fu.setCalificacion(rs.getInt(8));
-
-                funcionesDisponibles.add(fu);
+                    funcionesDisponibles.add(fu);
+                }
+            } catch (SQLException e){
+                throw new RuntimeException(e);
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
-        }
 
-        return funcionesDisponibles;
+            return funcionesDisponibles;
+        }
+        if (filtro.equals("Mejor calificado")){
+            ArrayList<BFuncion> listaFunMejorCalif = new ArrayList<>();
+            try {
+                String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
+                        "f.fecha,f.hora,\n" +
+                        "se.nombre_sede as `Sede`,\n" +
+                        "sa.idsala as `Sala`,\n" +
+                        "f.precio_ticket as `Precio`,\n" +
+                        "round(p.calificacion) as `Calificacion`\n" +
+                        "from funciones f\n" +
+                        "inner join salas sa on (f.idsala=sa.idsala)\n" +
+                        "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
+                        "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
+                        "order by p.calificacion desc\n" +
+                        "limit 1;";
+                Connection conn = DriverManager.getConnection(url,user,pass);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+
+                while (rs.next()){
+                    BFuncion fu = new BFuncion();
+                    fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
+                    fu.setPelicula(rs.getString(2));
+                    fu.setFecha(rs.getString(3));
+                    fu.setHora(rs.getString(4));
+                    fu.setSede(rs.getString(5));
+                    fu.setIdSala(rs.getInt(6));
+                    fu.setPrecioTicket(rs.getInt(7));
+                    fu.setCalificacion(rs.getInt(8));
+
+                    listaFunMejorCalif.add(fu);
+                }
+
+            } catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+            return listaFunMejorCalif;
+        }
+        if (filtro.equals("Mas visto")){
+            ArrayList<BFuncion> listaFunMasVis = new ArrayList<>();
+            try {
+                String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
+                        "f.fecha,f.hora,\n" +
+                        "se.nombre_sede as `Sede`,\n" +
+                        "sa.idsala as `Sala`,\n" +
+                        "f.precio_ticket as `Precio`,\n" +
+                        "round(p.calificacion) as `Calificacion`\n" +
+                        "from funciones f\n" +
+                        "inner join salas sa on (f.idsala=sa.idsala)\n" +
+                        "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
+                        "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
+                        "inner join compradefunciones cdf on (cdf.idfuncion=f.idfuncion)\n" +
+                        "order by cdf.asistencia desc\n" +
+                        "limit 1;";
+                Connection conn = DriverManager.getConnection(url,user,pass);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                System.out.println("PORBAAAAANDOOO WIWIWIWI");
+                while (rs.next()){
+                    BFuncion fu = new BFuncion();
+                    fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
+                    fu.setPelicula(rs.getString(2));
+                    fu.setFecha(rs.getString(3));
+                    fu.setHora(rs.getString(4));
+                    fu.setSede(rs.getString(5));
+                    fu.setIdSala(rs.getInt(6));
+                    fu.setPrecioTicket(rs.getInt(7));
+                    fu.setCalificacion(rs.getInt(8));
+
+                    listaFunMasVis.add(fu);
+                }
+
+            } catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+            return listaFunMasVis;
+        }
+        if (filtro.equals("Menos visto")){
+            ArrayList<BFuncion> listaFunMenVis = new ArrayList<>();
+            try {
+
+                String sql = "select f.idfuncion as 'IdFunción',p.nombre as `Título de Película`,\n" +
+                        "f.fecha,f.hora,\n" +
+                        "se.nombre_sede as `Sede`,\n" +
+                        "sa.idsala as `Sala`,\n" +
+                        "f.precio_ticket as `Precio`,\n" +
+                        "round(p.calificacion) as `Calificacion`\n" +
+                        "from funciones f\n" +
+                        "inner join salas sa on (f.idsala=sa.idsala)\n" +
+                        "inner join sedes se on (sa.nombre_sede=se.nombre_sede)\n" +
+                        "inner join peliculas p on (p.idpelicula=f.idpelicula)\n" +
+                        "inner join compradefunciones cdf on (cdf.idfuncion=f.idfuncion)\n" +
+                        "order by cdf.asistencia asc\n" +
+                        "limit 1;";
+                Connection conn = DriverManager.getConnection(url,user,pass);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+
+                while (rs.next()){
+                    BFuncion fu = new BFuncion();
+                    fu.setIdFuncion(Integer.parseInt(rs.getString(1)));
+                    fu.setPelicula(rs.getString(2));
+                    fu.setFecha(rs.getString(3));
+                    fu.setHora(rs.getString(4));
+                    fu.setSede(rs.getString(5));
+                    fu.setIdSala(rs.getInt(6));
+                    fu.setPrecioTicket(rs.getInt(7));
+                    fu.setCalificacion(rs.getInt(8));
+
+                    listaFunMenVis.add(fu);
+                }
+
+            } catch (SQLException e){
+                throw new RuntimeException(e);
+            }
+            return listaFunMenVis;
+        }
+        return null;
     }
+
+
     public ArrayList<BFuncion> obtenerFuncionMejorCalificada(){
         ArrayList<BFuncion> listaFunMejorCalif = new ArrayList<>();
         String user = "root";
@@ -137,6 +248,7 @@ public class OperadorDao {
             throw new RuntimeException(e);
         }
         return listaFunMejorCalif;
+
     }
     public ArrayList<BFuncion> obtenerFuncionMasVista(){
         ArrayList<BFuncion> listaFunMasVis = new ArrayList<>();
--- src/main/java/com/example/cinestudiar/servlets/OperadorServlet.java
+++ src/main/java/com/example/cinestudiar/servlets/OperadorServlet.java
@@ -16,20 +16,39 @@ public class OperadorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action = request.getParameter("action") == null ? "listarTodas" : request.getParameter("action");
+        String action = request.getParameter("action") == null ? "funciones" : request.getParameter("action");
         OperadorDao operadorDao = new OperadorDao();
-        RequestDispatcher view;
+        String filtro = request.getParameter("listarFunciones");
+        //if (filtro == "Funciones Disponibles") {
+         //
+        //}else {
+          //  ArrayList<BFuncion> lista = operadorDao.TodasLasFunciones();
+        //}
+        //ArrayList<BFuncion> lista = operadorDao.FuncionesDisponibles();
+        RequestDispatcher requestDispatcher;
         switch (action){
 
-
-
-            case "listarTodas" -> {
-
-                request.setAttribute("listarFunciones","");
-                request.setAttribute("todasLasFunciones", operadorDao.TodasLasFunciones());
-                RequestDispatcher requestDispatcher = request.getRequestDispatcher("Operador/Todas_func.jsp");
+            case "funciones" ->{
+                request.setAttribute("listarFunciones", "");
+                request.setAttribute("Funciones", operadorDao.filtradoFunciones(""));
+                requestDispatcher = request.getRequestDispatcher("Operador/Todas_func.jsp");
                 requestDispatcher.forward(request,response);
+                break;
             }
+
+            /*case " " -> {
+                //ArrayList<BFuncion> lista = null;
+                if (request.getParameter("listarFunciones")!=null){
+                    request.setAttribute("Funciones", lista);
+
+                }else{
+                    request.setAttribute("Funciones", operadorDao.filtradoFunciones(filtro, lista));
+                }
+                view= request.getRequestDispatcher("Operador/Todas_func.jsp");
+                view.forward(request,response);
+            }
+            */
+
             /*
             case "filtrado" ->{
 
@@ -78,40 +97,25 @@ public class OperadorServlet extends HttpServlet {
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String action = request.getParameter("action") == null ? "listarTodas" : request.getParameter("action");
+        //String action = request.getParameter("action") ;
+        String action = request.getParameter("action") == null ? "redireccionar" : request.getParameter("action");
         OperadorDao operadorDao = new OperadorDao();
-        RequestDispatcher view;
         String filtro;
-       // String action = request.getParameter("a") == null ? "listarTodas" : request.getParameter("a");
-        switch (action){
+        RequestDispatcher view;
+
+        //ArrayList<BFuncion> lista = operadorDao.TodasLasFunciones();
+
+
+         switch (action){
 
             case "filtro_func":
                 System.out.println("HOLAAA");
                 filtro = request.getParameter("listarFunciones");
-                try{
-                    if (filtro == "Funciones Disponibles"){
-                        request.setAttribute("funcionesDisponibles", operadorDao.FuncionesDisponibles());
-                        RequestDispatcher requestDispatcher=request.getRequestDispatcher("Operador/Func_Disp.jsp");
-                        requestDispatcher.forward(request,response);
-                    }
-                    if (filtro == "Mejor calificado"){
-                        request.setAttribute("listaFunMejorCalif", operadorDao.obtenerFuncionMejorCalificada());
-                        RequestDispatcher requestDispatcher=request.getRequestDispatcher("Operador/Todas_func.jsp");
-                        requestDispatcher.forward(request,response);
-                    }
-                    if (filtro == "Más visto"){
-                        request.setAttribute("listaFunMasVis", operadorDao.obtenerFuncionMasVista());
-                        RequestDispatcher requestDispatcher=request.getRequestDispatcher(("Operador/Todas_func.jsp"));
-                        requestDispatcher.forward(request,response);
-                    }
-                    if (filtro == "Menos visto"){
-                        request.setAttribute("listaFunMenVis", operadorDao.obtenerFuncionMenosVista());
-                        RequestDispatcher requestDispatcher=request.getRequestDispatcher(("Operador/Todas_func.jsp"));
-                        requestDispatcher.forward(request,response);
-                    }
-                }catch (NumberFormatException e){
-
-                }
+                request.setAttribute("Funciones", operadorDao.filtradoFunciones(filtro));
+                request.setAttribute("listarFunciones", filtro);
+                view= request.getRequestDispatcher("Operador/Todas_func.jsp");
+                view.forward(request,response);
+                break;
 
         }
     }
