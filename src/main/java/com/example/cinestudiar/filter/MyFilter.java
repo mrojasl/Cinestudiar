d97d032f306b9142a834d695df4757ed7403c086
Angel Bautista C <a20196044@pucp.edu.pe>
2022-07-18 05:16:16 -0500

    Filter calificacion

--- src/main/java/com/example/cinestudiar/filter/MyFilter.java
+++ src/main/java/com/example/cinestudiar/filter/MyFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(filterName = "MyFilter",servletNames = {"CarritoServlet","PerfilServlet","YaComproServlet","ServAdmin","inicioServlet","DetallesServlet","OperadorServlet"})
+@WebFilter(filterName = "MyFilter",servletNames = {"CarritoServlet","PerfilServlet","YaComproServlet","ServAdmin","inicioServlet","DetallesServlet","OperadorServlet","CalificacionServlet","ReporteServlet"})
 public class MyFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
     }
